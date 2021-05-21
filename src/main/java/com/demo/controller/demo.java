12703d1362276922a653fd725f6625ae7c951a6f
saul3 <saul3@LAPTOP-DGD5E4P3>
2021-05-20 21:37:59 -0500

    portada

---
+++ src/main/java/com/demo/controller/demo.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class demo {
+	
+	@GetMapping("/demo/portada")
+	public String portada() {
+		return "portada";
+	}
+
+}
